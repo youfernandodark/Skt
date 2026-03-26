65bedf9e7a2cf271a352e96948376838ece76419
youfernandodark <darkskull.adriask8@hotmail.com>
2026-03-26 19:55:22 -0300

    Add files via upload

--- organizador.py
+++ organizador.py
@@ -1,9 +1,9 @@
-# Lista de conteúdos com metadados detalhados
+# Lista de conteúdos com categorias bem definidas
 meu_vod = [
     {
         "id": "TerrorEstrada2024",
         "titulo": "Terror na Estrada",
-        "grupo": "Filmes",  # Mantém como Filmes
+        "grupo": "Filmes",  # Categoria de Filmes
         "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwFH4s4vcllU_up3cxXKTFeZjcjGZiQDxgjlf1igAIWw&s=10",
         "url": "https://video.wixstatic.com/video/db47f8_4671182ae2b347e888c6b5214bf9286d/720p/mp4/file.mp4",
         "ano": "2024",
@@ -12,10 +12,19 @@ meu_vod = [
     {
         "id": "LCDP_S01E01",
         "titulo": "La Casa de Papel - T01 E01: Efetuado",
-        "grupo": "Séries",  # ALTERADO: Agora será agrupado em Séries
+        "grupo": "Séries: La Casa de Papel",  # Alterado para forçar a categoria de série
         "logo": "https://m.media-amazon.com/images/M/MV5BN2Q2NmZjMzQtM2ZhYS00NWM3LTg0MDktYjNkNWYzM2MyYjI5XkEyXkFqcGc@._V1_.jpg",
         "url": "https://video.wixstatic.com/video/8d3786_251e5a43d9384b21ab6525a558d1d830/720p/mp4/file.mp4",
         "ano": "2017",
         "sinopse": "O Professor recruta uma jovem assaltante e outros sete criminosos para um roubo épico."
+    },
+    {
+        "id": "LCDP_S01E02",
+        "titulo": "La Casa de Papel - T01 E02: Imprudências",
+        "grupo": "Séries: La Casa de Papel", # Mantém o mesmo grupo para agrupar os episódios
+        "logo": "https://m.media-amazon.com/images/M/MV5BN2Q2NmZjMzQtM2ZhYS00NWM3LTg0MDktYjNkNWYzM2MyYjI5XkEyXkFqcGc@._V1_.jpg",
+        "url": "https://video.wixstatic.com/video/8d3786_dc87babb750b4600ab81da3ed110c8aa/720p/mp4/file.mp4",
+        "ano": "2017",
+        "sinopse": "A situação se complica dentro da Casa da Moeda."
     }
 ]
